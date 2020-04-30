73b039e6784d9e62e14332cbe07581f63f165a7a
FelipeCandian <34189423+felipecandian@users.noreply.github.com>
2020-04-30 00:08:56 -0300

    Mesclagem do sistema

---
+++ web/dados.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css"> 
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="icon" href="images/virus.png">    
+  
+    <title>Covid-19</title>
+  </head>
+  <body>
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">About</h4>
+              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Menu</h4>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white">Cidades</a></li>
+                <li><a href="#" class="text-white">Doação</a></li>
+                <li><a href="#" class="text-white">Mapa</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+              <strong><h6>COVID-19/MINAS GERAIS</h6></strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+      
+    <main role="main">
+        <div class="container">
+            <section class="jumbotron text-center p-0">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="Index.php">Buscar</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Alvinópolis</li>
+                </ol>
+                <h2 class="jumbotron-heading">Painel CoronaVírus</h2>
+                <p class="lead text-muted">Últimas Atualizaçãos</p>
+                <p class="subtext">08/04/2020</p>
+            </section>
+            <div class="row">
+            <div class="col-md-3">
+                <div class="card card-borda-azul animated bounceInUp slow">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col">    
+                            <h3 class="cor1">01</h3>
+                            <p class="subtext">Casos Suspeitos</p>
+                        </div>
+                        <div class="col">
+                            <img class="img" src="images/resultado.png" width="70" height="70"  align="right" alt="">   
+                        </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-3">
+                <div class="card card-borda-amarelo animated bounceInUp slow">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col">    
+                            <h3 class="cor2">0</h3>
+                            <p class="subtext">Casos Confirmados</p>
+                        </div>
+                        <div class="col">
+                            <img class="img" src="images/pesquisa.png" width="70" height="70"  align="right" alt="">   
+                        </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+            <div class="col-md-3">
+                <div class="card card-borda-verde animated bounceInUp slow">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col">    
+                            <h3 class="cor3">01</h3>
+                            <p class="subtext">Casos Descartados</p>
+                        </div>
+                        <div class="col">
+                            <img class="img" src="images/cancelar.png" width="70" height="70"  align="right" alt="">   
+                        </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+            <div class="col-md-3">
+                <div class="card card-borda-vermelho animated bounceInUp slow">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col">    
+                            <h3 class="cor4">0</h3>
+                            <p class="subtext">Casos de Óbitos</p>
+                        </div>
+                        <div class="col">
+                            <img class="img" src="images/certidao-de-obito.png" width="70" height="70"  align="right" alt="">   
+                        </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>    
+            </div>
+            
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card animated bounceInUp slow delay-1s">
+                        <div class="card-body">
+                            <h5 class="subtext">Mapa Unidade Saúde</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Encontre as mais próximas de você</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+           <!--<div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body" align="center">
+                            <img class="img" src="images/hospital.png"width="70" height="70" alt="">          
+                            <h5 class="subtext">Unidade Saúde</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Encontre as mais próximas de você</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body" align="center">
+                            <img class="img" src="images/hospital.png"width="70" height="70" alt="">          
+                            <h5 class="subtext">Unidade Saúde</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Encontre as mais próximas de você</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body" align="center">
+                            <img class="img" src="images/hospital.png"width="70" height="70" alt="">          
+                            <h5 class="subtext">Unidade Saúde</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Encontre as mais próximas de você</h6>
+                        </div>
+                    </div>
+                </div> 
+            </div> -->   
+        </div>
+    </main>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ web/pesquisa.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">  
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">About</h4>
+              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Menu</h4>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white">Cidades</a></li>
+                <li><a href="#" class="text-white">Doação</a></li>
+                <li><a href="#" class="text-white">Mapa</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+              <strong><h5>COVID-19/MINAS GERIAS</h5></strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+      
+    <main role="main">
+       
+    </main>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>

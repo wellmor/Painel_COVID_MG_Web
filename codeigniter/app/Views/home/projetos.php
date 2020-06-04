00cd4d04534f549bf50392e7ebb0d3ce09d9e7df
Ariel Granato <arielgranatob@gmail.com>
2020-06-04 01:21:54 -0300

    BD atualizado com microrregiões. Página de projeto estática finalizada

--- codeigniter/app/Views/home/projetos.php
+++ codeigniter/app/Views/home/projetos.php
@@ -12,41 +12,37 @@
 </head>
 
 <body>
-  <header>
-    <div class="collapse bg-dark" id="navbarHeader">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-8 col-md-7 py-4">
-            <h4 class="text-white">Sobre o projeto</h4>
-            <p class="text-muted">Painel de Informações e Emissão de Alertas no Enfrentamento ao COVID-19 nas Microrregiões de Ubá e Juiz de Fora</p>
-          </div>
-          <div class="col-sm-4 offset-md-1 py-4">
-            <h4 class="text-white">Menu</h4>
-            <ul class="list-unstyled">
-              <li><a href="/home/projetos" class="text-white">Projetos</a></li>
-              <li><a href="/home/dicas" class="text-white">Dicas</a></li>
-              <li><a href="#" class="text-white">Doação</a></li>
-              <li><a href="/admin" class="text-white">Login</a></li>
-            </ul>
-          </div>
+  <div class="collapse bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="text-white">Sobre o projeto</h4>
+          <p class="text-muted">Painel de Informações e Emissão de Alertas no Enfrentamento ao COVID-19 nas Microrregiões de Ubá e Juiz de Fora</p>
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+          <h4 class="text-white">Menu</h4>
+          <ul class="list-unstyled">
+            <li><a href="/home/projetos" class="text-white">Projetos</a></li>
+            <li><a href="/home/dicas" class="text-white">Dicas</a></li>
+            <li><a href="#" class="text-white">Doação</a></li>
+            <li><a href="/admin" class="text-white">Login</a></li>
+          </ul>
         </div>
       </div>
     </div>
-    <div class="navbar navbar-dark bg-dark box-shadow">
-      <div class="container d-flex justify-content-between">
-        <a href="#" class="navbar-brand d-flex align-items-center">
-          <strong>
-            <!-- <h6>COVID-19/MINAS GERAIS</h6> -->
-            <div class="container">
-              <img src="https://trello-attachments.s3.amazonaws.com/5e95f929e05def876f6b6218/5ebdcf3e42dfab04157c06c7/c8076c23f7c311be8870da68b7e08bf6/Logo.png" style="width: 192px; height: 56px">
-            </div>
-          </strong>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+  </div>
+  <div class="navbar navbar-dark bg-dark box-shadow">
+    <div class="container d-flex justify-content-between">
+      <div class="navbar-brand d-flex align-items-center">
+        <div class="container">
+          <img src="/assets/images/logo.png" style="width: 192px; height: 56px">
+        </div>
       </div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
     </div>
+  </div>
   </header>
 
   <main role="main">
@@ -57,20 +53,66 @@
       </ol>
       <section class="jumbotron text-center p-0">
         <h2 class="jumbotron-heading">Projetos</h2>
-        <p class="lead text-muted">Confira a seguir as ações desenvolvidas no Campus</p>
+        <p class="lead text-muted">Confira a seguir algumas ações desenvolvidas no <b>Campus Rio Pomba</b></p>
       </section>
+
       <div class="row">
         <div class="col-md-4">
           <div class="card animated bounceInUp slow delay-0s">
-            <div class="card-body" align="center">
-              <img src="/assets/images/logo-maos-dadas.png" width="" height="90" alt="">
+            <div class="card-body text-center">
+              <img style="border-radius:10px;" src="/assets/images/maosdadas.png" width="" height="90" alt="Projeto Mãos Dadas">
               <h5 class="subtext mt-2">Projeto Mãos Dadas</h5>
-              <a href="http://projetomaosdadas.emcomp.com.br" target="_blank" type="button" class="btn btn-outline-dark btn-block">Ir Para Site</a>
+              <a href="http://projetomaosdadas.emcomp.com.br" target="_blank" type="button" class="btn btn-outline-dark btn-block">Mais informações</a>
             </div>
           </div>
         </div>
+        <div class="col-md-4">
+          <div class="card animated bounceInUp slow delay-0s">
+            <div class="card-body text-center">
+              <img style="border-radius:10px;" src="/assets/images/maosahorta.png" width="" height="90" alt="Ações da Rede Mãos à Horta em tempos de COVID19 (imagem não oficial)">
+              <h5 style="font-size: 16px;">Ações da Rede Mãos à Horta<br>em tempos de COVID19</h5>
+              <a href="https://www.ifsudestemg.edu.br/noticias/riopomba/2020/04/projetos-mostram-importancia-do-campus-rio-pomba-para-regiao-na-epoca-de-pandemia" target="_blank" type="button" class="btn btn-outline-dark btn-block">Mais informações</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card animated bounceInUp slow delay-0s">
+            <div class="card-body text-center">
+              <img style="border-radius:10px;" src="/assets/images/alcoolgel.png" width="" height="90" alt="Elaboração de álcool gel em tempos de COVID19 (imagem não oficial)">
+              <h5 style="font-size: 16px;">Elaboração de álcool gel<br>em tempos de COVID19</h5>
+              <a href="https://www.ifsudestemg.edu.br/noticias/riopomba/2020/04/projetos-mostram-importancia-do-campus-rio-pomba-para-regiao-na-epoca-de-pandemia" target="_blank" type="button" class="btn btn-outline-dark btn-block">Mais informações</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card animated bounceInUp slow delay-0s">
+            <div class="card-body text-center">
+              <img style="border-radius:10px;" src="/assets/images/rodapsicologo.png" width="" height="90" alt="Psicólogos promovem Rodas de Conversa On-line com alunos do IF Sudeste MG (imagem não oficial)">
+              <h5 style="font-size: 16px;">Psicólogos promovem Rodas de Conversa<br>On-line com alunos do IF Sudeste MG</h5>
+              <a href="https://www.ifsudestemg.edu.br/noticias/riopomba/2020/05/psicologos-promovem-rodas-de-conversa-on-line-com-alunos-do-if-sudeste-mg" target="_blank" type="button" class="btn btn-outline-dark btn-block">Mais informações</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card animated bounceInUp slow delay-0s">
+            <div class="card-body text-center">
+              <img style="border-radius:10px;" src="/assets/images/legislacao.png" width="" height="90" alt="Legislação acessível em tempos de pandemia (imagem não oficial)">
+              <h5 style="font-size: 17px;">Legislação acessível em tempos de pandemia</h5>
+              <a href="https://www.ifsudestemg.edu.br/riopomba/institucional/extensao/projetos-e-programas/projetos-edital-2020-06/legislacao-acessivel-em-tempos-de-pandemia" target="_blank" type="button" class="btn btn-outline-dark btn-block">Mais informações</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card animated bounceInUp slow delay-0s">
+            <div class="card-body text-center">
+              <img style="border-radius:10px;" src="/assets/images/consambiental.png" width="" height="90" alt="Conscientização ambiental em tempos de Covid-19 (imagem não oficial)">
+              <h5 style="font-size: 17px;">Conscientização ambiental em tempos de Covid-19</h5>
+              <a href="https://www.ifsudestemg.edu.br/noticias/riopomba/2020/05/conscientizacao-ambiental-em-tempos-de-covid-e-tema-de-projeto-de-extensao" target="_blank" type="button" class="btn btn-outline-dark btn-block">Mais informações</a>
+            </div>
+          </div>
+        </div>
+        <!-- FONTE: https://app.powerbi.com/view?r=eyJrIjoiNzdiMjg2MmEtMmQ1Zi00YzZkLTgwNmMtYzFmMGNmZWIzZDNiIiwidCI6IjAwZjhmMzk0LTU4N2UtNGQ0ZC1hZGNmLWQ2OGIwNzBlMGEwZCJ9 -->
       </div>
-    </div>
   </main>
 
   <!-- Optional JavaScript -->
